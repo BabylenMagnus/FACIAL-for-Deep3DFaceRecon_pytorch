5a70a8bd059b3c203ea8fb3ca334580bc92d6a3f
Evgeniy <sevostyanikhin02@gmail.com>
2022-02-21 07:26:00 +0300

    start training

--- audio2face/fintuning2-trainheadpose.py
+++ audio2face/fintuning2-trainheadpose.py
@@ -20,15 +20,12 @@ opt = parser.parse_args()
 if not os.path.exists(opt.savepath):
     os.mkdir(opt.savepath)
 
-audio_paths = []
-audio_paths.append(opt.audiopath)
-npz_paths = []
-npz_paths.append(opt.npzpath)
-cvs_paths = []
-cvs_paths.append(opt.cvspath)
+audio_paths = [opt.audiopath]
+npz_paths = [opt.npzpath]
+cvs_paths = [opt.cvspath]
 
-batchsz = 16
-epochs = 11
+batch_size = 512
+epochs = 20
 
 device = torch.device('cuda')
 torch.manual_seed(1234)
@@ -36,7 +33,7 @@ torch.manual_seed(1234)
 training_set = Facial_Dataset(audio_paths, npz_paths, cvs_paths)
 
 train_loader = DataLoader(training_set,
-                          batch_size=batchsz,
+                          batch_size=batch_size,
                           shuffle=True,
                           drop_last=True,
                           pin_memory=True)
@@ -59,29 +56,29 @@ def weights_init(m):
 
 def main():
     lr = 1e-4
-    modelgen = TfaceGAN().to(device)
+    model_gen = TfaceGAN().to(device)
 
     modeldis = NLayerDiscriminator().to(device)
 
-    modelgen.load_state_dict(torch.load(opt.pretainpath_gen))
-    print(modelgen)
+    model_gen.load_state_dict(torch.load(opt.pretainpath_gen))
+    print(model_gen)
     print(modeldis)
 
-    optimG = optim.Adam(modelgen.parameters(), lr=lr * 0.1)
+    optim_g = optim.Adam(model_gen.parameters(), lr=lr * 0.1)
 
-    optimD = optim.Adam(modeldis.parameters(), lr=lr * 0.1)
+    optim_d = optim.Adam(modeldis.parameters(), lr=lr * 0.1)
 
-    criteon1 = nn.L1Loss()
-    criteon = nn.MSELoss()
+    l1_loss = nn.L1Loss()
+    mse_loss = nn.MSELoss()
 
     for epoch in range(0, epochs):
 
         if epoch % 5 == 0:
-            torch.save(modelgen.state_dict(), opt.savepath + '/Gen-' + str(epoch) + '.mdl')
+            torch.save(model_gen.state_dict(), opt.savepath + '/Gen-' + str(epoch) + '.mdl')
             torch.save(modeldis.state_dict(), opt.savepath + '/Dis-' + str(epoch) + '.mdl')
 
         for step, (x, y) in enumerate(train_loader):
-            modelgen.train()
+            model_gen.train()
             # x(64, 128, 29) y(64, 128, 70)
             x, y = x.to(device), y.to(device)
             motiony = y[:, 1:, :] - y[:, :-1, :]
@@ -90,42 +87,42 @@ def main():
             set_requires_grad(modeldis, True)
 
             predr = modeldis(torch.cat([y, motiony], 1))
-            lossr = criteon(torch.ones_like(predr), predr)
+            lossr = mse_loss(torch.ones_like(predr), predr)
 
-            yf = modelgen(x, y[:, :1, :])
+            yf = model_gen(x, y[:, :1, :])
             motionlogits = yf[:, 1:, :] - yf[:, :-1, :]
 
             predf = modeldis(torch.cat([yf, motionlogits], 1).detach())
-            lossf = criteon(torch.zeros_like(predf), predf)
+            lossf = mse_loss(torch.zeros_like(predf), predf)
 
-            lossD = lossr + lossf
-            optimD.zero_grad()
-            lossD.backward()
-            optimD.step()
+            loss_d = lossr + lossf
+            optim_d.zero_grad()
+            loss_d.backward()
+            optim_d.step()
 
             # generator
             set_requires_grad(modeldis, False)
-            loss_s = 10 * (criteon1(yf[:, :1, :6], y[:, :1, :6]) + criteon1(yf[:, :1, 6], y[:, :1, 6]) + criteon1(
+            loss_s = 10 * (l1_loss(yf[:, :1, :6], y[:, :1, :6]) + l1_loss(yf[:, :1, 6], y[:, :1, 6]) + l1_loss(
                 yf[:, :1, 6:], y[:, :1, 6:]))
-            lossg_e = 20 * criteon(yf[:, :, 7:], y[:, :, 7:])
-            lossg_em = 200 * criteon(motionlogits[:, :, 7:], motiony[:, :, 7:])
+            loss_ge = 20 * mse_loss(yf[:, :, 7:], y[:, :, 7:])
+            lossg_em = 200 * mse_loss(motionlogits[:, :, 7:], motiony[:, :, 7:])
 
-            loss_au = 0.5 * criteon(yf[:, :, 6], y[:, :, 6])
-            loss_aum = 1 * criteon(motionlogits[:, :, 6], motiony[:, :, 6])
-            loss_pose = 1 * criteon(yf[:, :, :6], y[:, :, :6])
-            loss_posem = 10 * criteon(motionlogits[:, :, :6], motiony[:, :, :6])
+            loss_au = 0.5 * mse_loss(yf[:, :, 6], y[:, :, 6])
+            loss_aum = 1 * mse_loss(motionlogits[:, :, 6], motiony[:, :, 6])
+            loss_pose = 1 * mse_loss(yf[:, :, :6], y[:, :, :6])
+            loss_posem = 10 * mse_loss(motionlogits[:, :, :6], motiony[:, :, :6])
             predf2 = modeldis(torch.cat([yf, motionlogits], 1))
 
-            lossg_gan = criteon(torch.ones_like(predf2), predf2)
+            lossg_gan = mse_loss(torch.ones_like(predf2), predf2)
 
-            lossG = loss_s + lossg_e + lossg_em + loss_au + loss_aum + loss_pose + loss_posem + 0.1 * lossg_gan
+            loss_g = loss_s + loss_ge + lossg_em + loss_au + loss_aum + loss_pose + loss_posem + 0.1 * lossg_gan
             # lossG =   loss_pose_1 + loss_pose_2
-            optimG.zero_grad()
-            lossG.backward()
-            optimG.step()
+            optim_g.zero_grad()
+            loss_g.backward()
+            optim_g.step()
 
-            if step % 60 == 0:
-                print('epoch: ', epoch, ' loss_s: ', loss_s.item(), ' lossg_e: ', lossg_e.item(), ' lossg_em: ',
+            if step % 8 == 0:
+                print('epoch: ', epoch, ' loss_s: ', loss_s.item(), ' lossg_e: ', loss_ge.item(), ' lossg_em: ',
                       lossg_em.item())
                 print(' loss_au: ', loss_au.item(), ' loss_aum: ', loss_aum.item())
                 print(' loss_pose: ', loss_pose.item(), ' loss_posem: ', loss_posem.item())
